d28d1fc835227e03012782a69cd3e75cf174bcb2
Matheus Cavalcanti Rebola <m.cavalcantir@outlook.com>
2023-09-29 21:31:15 -0300

    first commit

---
+++ src/main/java/github/com/matheusrebola/ecommerceAPI/EcommerceApplication.java
@@ -0,0 +1,13 @@
+package github.com.matheusrebola.ecommerceAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceApplication.class, args);
+	}
+
+}
