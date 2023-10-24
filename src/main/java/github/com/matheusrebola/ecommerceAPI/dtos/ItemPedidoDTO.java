deef288a443272b140882d54216c9bb1b4923379
Matheus Rebola <58261178+matheusrebola@users.noreply.github.com>
2023-10-24 03:51:42 +0000

    feat: configuraçao parcialdas DTOs

--- src/main/java/github/com/matheusrebola/ecommerceAPI/dtos/ItemPedidoDTO.java
+++ src/main/java/github/com/matheusrebola/ecommerceAPI/dtos/ItemPedidoDTO.java
@@ -7,16 +7,12 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class ClienteDTO {
+public class ItemPedidoDTO {
 	private Long id;
-	private String nome;
-	private String cep;
-	private String logradouro;
-	private String numero;
-	private String complemento;
-	private String bairro;
-	private String municipio;
-	private String uf;
-	private Instant dataCadastro;
+	private Pedido pedido;
+	private Produto produto;
+	private BigDecimal quantidade;
+	private BigDecimal valorUnitario;
+	private BigDecimal valorTotal;
 	private boolean ativo;
 }
