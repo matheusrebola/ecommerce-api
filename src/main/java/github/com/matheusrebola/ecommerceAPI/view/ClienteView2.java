db636f5ab7e7ae1a3b2c9d13f08182e6c07aab9d
matheus rebola <m.cavalcantir@outlook.com>
2023-11-20 11:49:42 -0300

    feat: configuração das views

--- src/main/java/github/com/matheusrebola/ecommerceAPI/view/ClienteView2.java
+++ src/main/java/github/com/matheusrebola/ecommerceAPI/view/ClienteView2.java
@@ -1,4 +1,14 @@
 package github.com.matheusrebola.ecommerceAPI.view;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public interface ClienteView2 {
+    Long getId();
+
+    String getNome();
+
+    @Value("#{target.municipio + ' - ' + target.uf + ' - ' + target.cep + ' - ' + target.logradouro + ' - ' + target.numero}")
+    String getEnderecoCompleto();
+
+
 }
