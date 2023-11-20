d88e597884c0b3e3d0e7209aa08874870b265d3c
matheus rebola <m.cavalcantir@outlook.com>
2023-11-20 11:49:18 -0300

    feat: atualização das repositorys

--- src/main/java/github/com/matheusrebola/ecommerceAPI/repository/ClienteRepository.java
+++ src/main/java/github/com/matheusrebola/ecommerceAPI/repository/ClienteRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import github.com.matheusrebola.ecommerceAPI.model.Cliente;
 
+import java.util.List;
+
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+    <T> List<T> findByUf(String uf, Class<T> type);
 }
--- src/main/java/github/com/matheusrebola/ecommerceAPI/repository/PedidoRepository.java
+++ src/main/java/github/com/matheusrebola/ecommerceAPI/repository/PedidoRepository.java
@@ -1,9 +1,15 @@
 package github.com.matheusrebola.ecommerceAPI.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import github.com.matheusrebola.ecommerceAPI.model.Pedido;
 
+import java.util.List;
+
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+    @Query("select o from Pedido o where o.cliente.id = :id")
+    List<Pedido> findByCliente(@Param("id") Long id);
 }
