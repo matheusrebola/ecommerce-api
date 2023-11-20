b563b536148674c3f6470c3eb4fb28f8b3a76c0b
matheus rebola <m.cavalcantir@outlook.com>
2023-11-20 17:25:31 -0300

    feat: configuração da mapper de pedido e produto

--- src/main/java/github/com/matheusrebola/ecommerceAPI/mapper/PedidoMapper.java
+++ src/main/java/github/com/matheusrebola/ecommerceAPI/mapper/PedidoMapper.java
@@ -1,17 +1,22 @@
 package github.com.matheusrebola.ecommerceAPI.mapper;
 
+import github.com.matheusrebola.ecommerceAPI.dtos.PedidoCreateDTO;
 import github.com.matheusrebola.ecommerceAPI.dtos.PedidoDTO;
 import github.com.matheusrebola.ecommerceAPI.model.Pedido;
+
+
 import org.modelmapper.ModelMapper;
 
 public class PedidoMapper {
-    private ModelMapper modelMapper;
+    private final ModelMapper modelMapper;
+
+    public Pedido map(PedidoCreateDTO dto) {
+    	Pedido pedido = modelMapper.map(dto, Pedido.class);
+        return pedido;
+    }
 
     public PedidoDTO map(Pedido pedido) {
-        PedidoDTO dto = modelMapper.map(pedido, PedidoDTO.class);
-
-        dto.setCliente_id(pedido.getCliente().getId());
-
+    	PedidoDTO dto = modelMapper.map(pedido, PedidoDTO.class);
         return dto;
     }
 }
--- src/main/java/github/com/matheusrebola/ecommerceAPI/mapper/ProdutoMapper.java
+++ src/main/java/github/com/matheusrebola/ecommerceAPI/mapper/ProdutoMapper.java
@@ -1,4 +1,25 @@
 package github.com.matheusrebola.ecommerceAPI.mapper;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import github.com.matheusrebola.ecommerceAPI.dtos.ProdutoCreateDTO;
+import github.com.matheusrebola.ecommerceAPI.dtos.ProdutoDTO;
+import github.com.matheusrebola.ecommerceAPI.model.Produto;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
 public class ProdutoMapper {
+	private final ModelMapper modelMapper;
+
+    public Produto map(ProdutoCreateDTO dto) {
+    	Produto produto = modelMapper.map(dto, Produto.class);
+        return produto;
+    }
+
+    public ProdutoDTO map(Produto produto) {
+    	ProdutoDTO dto = modelMapper.map(produto, ProdutoDTO.class);
+        return dto;
+    }
 }
